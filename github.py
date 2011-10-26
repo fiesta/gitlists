ee2a83dbfbf2ae629874f5af7cb0a61c3d0dc655
Mike Dirolf <mike@dirolf.com>
2011-10-26 17:23:58 -0400

    Test and fix for missing email key

--- github.py
+++ github.py
@@ -90,7 +90,6 @@ def make_request(u, big=False):
             else:
                 raise
     res = json.loads(data)
-    print repr(res)
     if "error" in res:
         if "Rate Limit" in res["error"]:
             raise RateLimited()
@@ -100,10 +99,11 @@ def make_request(u, big=False):
 
 def current_user():
     data = make_request("/user")
-    if data and data["email"]:
+    if data and data.get("email", None):
         db.save_user(data["login"], data["email"], data["name"])
     elif data:
         email = make_request("/user/emails")[0]
+        data["email"] = email
         db.save_user(data["login"], email, data["name"])
     return data
 
--- test/test_www.py
+++ test/test_www.py
@@ -1,11 +1,30 @@
+import json
+import re
+import StringIO
 import sys
 import unittest
+import urllib
 sys.path[0:0] = [""]
 
 import webtest
 
 import db
 import www
+
+
+GITHUB = {}
+
+
+def monkey_patch_urllib():
+    def our_urlopen(url, params=None):
+        response = ""
+        match = re.match(r"^https://api\.github\.com(/.*)\?.+$", url)
+        if url.startswith("https://github.com/login/oauth/access_token"):
+            response = '{"access_token": ["dummy"]}'
+        elif match:
+            response = json.dumps(GITHUB.get(match.group(1)))
+        return StringIO.StringIO(response)
+    urllib.urlopen = our_urlopen
 
 
 class BaseTest(unittest.TestCase):
@@ -33,6 +52,11 @@ class BaseTest(unittest.TestCase):
 class TestWWW(BaseTest):
 
     def setUp(self):
+        global GITHUB
+        GITHUB = {}
+
+        monkey_patch_urllib()
+
         self.db = db.db
         for c in self.db.collection_names():
             if not c.startswith("system."):
@@ -56,3 +80,13 @@ class TestWWW(BaseTest):
         res.form["github"] = "mdirolf"
         res = self.follow(res.form.submit())
         self.assertIn("Thanks mdirolf", res)
+
+    def test_github_auth_without_email(self):
+        global GITHUB
+        GITHUB = {"/user": {"name": "Mike Dirolf", "login": "mdirolf"},
+                  "/user/emails": ["mike@example.com"],
+                  "/user/orgs": [],
+                  "/user/repos": []}
+
+        # Just make sure we don't get an exception here...
+        self.follow(self.app.get("/auth/github?code=dummy"))
